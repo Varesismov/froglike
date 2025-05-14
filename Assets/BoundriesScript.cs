08be3fbd3c554e787fe0cf6c77cc85464516a5b7
Krzysztof Stajniak <74303919+Varesismov@users.noreply.github.com>
2025-05-15 01:36:19 +0200

    Boundries
    playing with boundries and testing

--- Assets/BoundriesScript.cs
+++ Assets/BoundriesScript.cs
@@ -7,20 +7,30 @@ public class BoundriesScript : MonoBehaviour
     void Start()
     {
         Camera cam = Camera.main;
-        float height = 2f * cam.orthographicSize;
-        float width = height * cam.aspect;
+        //float height = 2f * cam.orthographicSize;
+        //float width = height * cam.aspect;
+        float width = 16f;
+        float height = 9f;
 
         Vector3 camPos = cam.transform.position;
 
-        // Boundries
-        CreateBoundary(new Vector2(camPos.x, camPos.y + height / 2 + thickness / 2),
-            new Vector2(width, thickness)); // Top
-        CreateBoundary(new Vector2(camPos.x, camPos.y - height / 2 - thickness / 2),
-            new Vector2(width, thickness)); // Bottom
-        CreateBoundary(new Vector2(camPos.x - width / 2 - thickness / 2, camPos.y),
-            new Vector2(thickness, height)); // Left
-        CreateBoundary(new Vector2(camPos.x + width / 2 + thickness / 2, camPos.y),
-            new Vector2(thickness, height)); // Right
+        //// Boundries
+        //CreateBoundary(new Vector2(camPos.x, camPos.y + height / 2 + thickness / 2),
+        //    new Vector2(width, thickness)); // Top
+
+        //CreateBoundary(new Vector2(camPos.x, camPos.y - height / 2 - thickness / 2),
+        //    new Vector2(width, thickness)); // Bottom
+        //CreateBoundary(new Vector2(camPos.x - width / 2 - thickness / 2, camPos.y),
+        //    new Vector2(thickness, height)); // Left
+        //CreateBoundary(new Vector2(camPos.x + width / 2 + thickness / 2, camPos.y),
+        //    new Vector2(thickness, height)); // Right
+
+
+        CreateBoundary(new Vector2(0, height / 2 + thickness / 2), new Vector2(width, thickness));  // Top
+        CreateBoundary(new Vector2(0, -height / 2 - thickness / 2), new Vector2(width, thickness)); // Bottom
+        CreateBoundary(new Vector2(-width / 2 - thickness / 2, 0), new Vector2(thickness, height)); // Left
+        CreateBoundary(new Vector2(width / 2 + thickness / 2, 0), new Vector2(thickness, height));  // Right
+
     }
     void CreateBoundary(Vector2 position, Vector2 size)
     {
@@ -30,7 +40,7 @@ public class BoundriesScript : MonoBehaviour
 
         BoxCollider2D collider = boundary.AddComponent<BoxCollider2D>();
         collider.size = size;
-        collider.isTrigger = false; // fizyczna przeszkoda
+        collider.isTrigger = false;
     }
 
     // Update is called once per frame
